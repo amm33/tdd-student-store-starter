b95f55b80f5427385e2175b8cf117c6fb8b029e9
Adriana Morales <adriana.morales@internal.salesforce.com>
2022-06-22 22:25:27 -0700

    added state variables

--- student-store-ui/src/components/App/App.jsx
+++ student-store-ui/src/components/App/App.jsx
@@ -15,9 +15,17 @@ import { useEffect } from "react";
 import axios from "axios";
 
 export default function App() {
+  //state variables
+  const [products, setProducts] = useState([]); //array of product objects, empty initially
+  const [isFetching, setIsFetching] = useState(null); //boolean; represents whether App is currently fetching products from the API
+  const [error, setError] = useState(null); //display message when something goes wrong with API requests
+  const [isOpen, setIsOpen] = useState(null); //represents whether the sidebar is open or not
+  const [shoppingCart, setShoppingCart] = useState([]); //store state for users shopping cart (what they want and the quantity)
+  const [checkoutForm, setCheckoutForm] = useState(null); //users info which will be sent to the API at checkout
+
   //getting the data
   const url = `https://codepath-store-api.herokuapp.com/store`;
-  const [products, setProducts] = useState([]);
+
   useEffect(() => {
     axios
       .get(url)
