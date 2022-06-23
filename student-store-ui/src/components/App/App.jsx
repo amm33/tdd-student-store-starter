a727a281cbef716df7c5ca856e961f85d0d78cfd
Adriana Morales <adriana.morales@internal.salesforce.com>
2022-06-23 10:16:03 -0700

    added event handlers that will be worked on

--- student-store-ui/src/components/App/App.jsx
+++ student-store-ui/src/components/App/App.jsx
@@ -13,6 +13,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 import { useEffect } from "react";
 import axios from "axios";
+import { useParams } from "react-router-dom";
 
 export default function App() {
   //state variables
@@ -30,13 +31,44 @@ export default function App() {
     axios
       .get(url)
       .then((response) => {
-        setProducts(response.data);
-        console.log(response.data);
+        products = setProducts(response.data);
+        console.log(products);
       })
       .catch((err) => {
-        console.log(err);
+        error = setError(err);
+        console.log(error);
       });
   }, []);
+
+  //pulling id from the route --check slides
+
+  //------------work on these-----------//
+  //event handlers to be passed as props to Home and Product Detail
+  handleOnToggle = () => {
+    //toggle open/close state of the sidebar
+  };
+
+  handleAddItemToCart = (productId) => {
+    //add product to shopping cart and make quantity 1
+    //if produce is already there, increase quantity by 1
+    //add price of product to the total in shopping cart
+  };
+
+  handleRemoveItemFromCart = (productId) => {
+    //decrease quantity in shopping cart by 1 - only if it exists
+    //if product does not exist, nothing should happen
+    //if new quantity is 0, it should be removed from the shopping cart
+  };
+
+  handleOnCheckoutFormChange = (name, value) => {
+    //update checkout form
+  };
+
+  handleOnSubmit = () => {
+    //look at the readme file
+    //should submit user order to API
+  };
+
   //---------------------------
   return (
     <div className="app">
