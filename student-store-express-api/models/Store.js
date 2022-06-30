3303220648221905c28192e09e78e67372f4f31d
Adriana Morales <adriana.morales@internal.salesforce.com>
2022-06-29 18:15:04 -0700

    Got the data

---
+++ student-store-express-api/models/Store.js
@@ -0,0 +1,24 @@
+// const { storage } = require("../data/storage");
+
+// class Storage {
+//   static getProducts() {
+//     return storage.get("products");
+//   }
+
+//   static getProductsWithId(Id) {
+//     return;
+//   }
+// }
+// // module.exports = Store;
+
+// const { storage } = require("../data/storage");
+
+// class Storage {
+//   static getProducts() {
+//     return storage.get("products");
+//   }
+
+//   static getProductById(id) {
+//     return storage.get("products".find({ id: Number(id) })).value();
+//   }
+// }
